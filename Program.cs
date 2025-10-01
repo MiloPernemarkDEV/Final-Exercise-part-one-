824786f67630d80446d74e91a0122c653573c89a
MiloPernemarkDEV <milo.pernemark@student.forsbergsskola.se>
2025-10-01 13:38:18 +0200

    Update Program.cs
    Added the formatting,

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Please Write an amount of seconds: ");
+Console.WriteLine("Please Write an amount of seconds: ");
 int input = Convert.ToInt32(Console.ReadLine());
 
 int days = input / 86400;
@@ -7,8 +7,13 @@ int minutes = (input % 3600) / 60;
 int seconds = input % 60;
 double daysExact = input / 86400.0;
 
-Console.WriteLine($"Seconds: {seconds}");
-Console.WriteLine($"Minutes: {minutes}");
-Console.WriteLine($"Hours: {hours}");
 Console.WriteLine($"Days: {days}");
-Console.WriteLine($"In total that's: {daysExact} days.");
+Console.WriteLine($"Hours: {hours}");
+Console.WriteLine($"Minutes: {minutes}");
+Console.WriteLine($"Seconds: {seconds}");
+
+
+Console.WriteLine($"Formatted: {days}.{hours:D2}:{minutes:D2}:{seconds:D2}");
+
+
+Console.WriteLine($"In total that's: {daysExact:F5} days.");
